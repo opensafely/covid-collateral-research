367c5176c7525204fa90578925effa44e0fb6cbb
Ruth Costello <ruth.costello@lshtm.ac.uk>
2022-05-13 16:55:43 +0100

    fixed script

--- analysis/study_definition_survival_pre.py
+++ analysis/study_definition_survival_pre.py
@@ -57,5 +57,171 @@ study = StudyDefinition(
         date_format="YYYY-MM-DD",
         return_expectations={"date": {"earliest": "2018-03-01"}},
     ),
+     # Stroke
+    stroke_primary_admission=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=stroke_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+    ),
+    # Heart failure
+    heart_failure_primary_admission=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=heart_failure_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+    ),
+    # VTE
+    vte_primary_admission=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=vte_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+    ),
+    cvd_admission=patients.satisfying(
+        """
+        mi_primary_admission OR
+        stroke_primary_admission OR
+        heart_failure_primary_admission OR
+        vte_primary_admission
+        """
+    ),
+     # Hospitalisation with primary reason diabetes (T1, T2 or ketoacidosis)
+    # Type 1 DM
+    t1dm_admission_primary=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=t1dm_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+    ),
+    # Type 2 DM
+    t2dm_admission_primary=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=t2dm_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+    ),
+    # Ketoacidosis
+    dm_keto_admission_primary=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=dm_keto_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+    ),
+    dm_admission=patients.satisfying(
+        """
+        t1dm_admission_primary OR
+        t2dm_admission_primary OR
+        dm_keto_admission_primary
+        """
+    ),
+    # Hospitalisation for COPD exacerbation
+    # Hospital admission - COPD exacerbation
+    copd_exacerbation=patients.satisfying(
+        """
+        copd_exacerbation_hospital OR 
+        copd_hospital OR 
+        (lrti_hospital AND copd_any)
+        """,
+        copd_exacerbation_hospital=patients.admitted_to_hospital(
+            with_these_diagnoses=copd_exacerbation_icd_codes,
+            on_or_before="2020-03-22",
+            returning="date_admitted",
+            find_first_match_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={"date": {"earliest": "2018-03-01"}},
+        ),
+        copd_hospital=patients.admitted_to_hospital(
+            with_these_primary_diagnoses=copd_icd_codes,
+            on_or_before="2020-03-22",
+            returning="date_admitted",
+            find_first_match_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={"date": {"earliest": "2018-03-01"}},
+            ),
+        lrti_hospital=patients.admitted_to_hospital(
+            with_these_primary_diagnoses=lrti_icd_codes,
+            on_or_before="2020-03-22",
+            returning="date_admitted",
+            find_first_match_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={"date": {"earliest": "2018-03-01"}},
+            ),
+        copd_any=patients.admitted_to_hospital(
+            with_these_diagnoses=copd_icd_codes,
+            on_or_before="2020-03-22",
+            returning="date_admitted",
+            find_first_match_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={"date": {"earliest": "2018-03-01"}},
+            ),
+        ),
+    asthma_exacerbation=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=asthma_exacerbation_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+        ),
+    resp_admission=patients.satisfying(
+        """
+        copd_exacerbation OR
+        asthma_exacerbation
+        """
+    ),
+    # Hospital admissions - mental health
+    depression_primary_admission=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=depression_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+        ),
+    anxiety_primary_admission=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=anxiety_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+        ),
+    smi_primary_admission=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=severe_mental_illness_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+        ),
+    self_harm_primary_admission=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=self_harm_icd_codes,
+        on_or_before="2020-03-22",
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+        ),
+    mh_admission=patients.satisfying(
+        """
+        depression_primary_admission OR
+        anxiety_primary_admission OR
+        smi_primary_admission OR
+        self_harm_primary_admission
+        """
+    ),
     **common_variables
 )
