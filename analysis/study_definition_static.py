5a2673b38be28bd1ebc65968806b04c5c30ec2c9
Ruth Costello <ruth.costello@lshtm.ac.uk>
2022-05-25 14:32:46 +0100

    Update study_definition_static.py

--- analysis/study_definition_static.py
+++ analysis/study_definition_static.py
@@ -14,13 +14,6 @@ study = StudyDefinition(
     index_date="2018-03-01",
     population=patients.all(),
 
-    age=patients.age_as_of(
-        "index_date",
-        return_expectations={
-            "rate": "universal",
-            "int": {"distribution": "population_ages"},
-        },
-    ),
     # Sex
     sex=patients.sex(
         return_expectations={
@@ -85,25 +78,6 @@ study = StudyDefinition(
                 "4": 0.19,
                 "5": 0.19,
                 }
-            },
-        },
-    ),
-    region=patients.registered_practice_as_of(
-    "index_date",
-    returning="nuts1_region_name",
-    return_expectations={
-        "rate": "universal",
-        "category": {
-            "ratios": {
-                "North East": 0.1,
-                "North West": 0.1,
-                "Yorkshire and the Humber": 0.1,
-                "East Midlands": 0.1,
-                "West Midlands": 0.1,
-                "East of England": 0.1,
-                "London": 0.2,
-                "South East": 0.2,
-                },
             },
         },
     ),
